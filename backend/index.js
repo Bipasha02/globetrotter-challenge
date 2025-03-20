a842260896d3ad5cff886e4db1e05a9ae41161b5
Bipasha Kumar <79408081+Bipasha02@users.noreply.github.com>
2025-03-20 23:48:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,11 @@ const app = express();
 // const port = process.env.PORT || 5000;
 const port = 5001;
 
-app.use(cors());
+app.use(cors(
+  {
+  origin: 'http://localhost:3000' 
+}
+));
 app.use(express.json());
 
 const destinations = [
